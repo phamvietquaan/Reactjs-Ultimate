1fe249adac995822ca213722e10877a999850232
phamvietquaan <quanpham4908@gmail.com>
2025-06-02 11:40:46 +0700

    video34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,14 @@
+const TodoData = (props) => {
+    //props la mot object
+    const { name, age, data } = props;
+    console.log(">>> check props:", props)
+    return (
+        <div className='todo-data'>
+            <div>My name is {props.name}</div>
+            <div >Learning React</div>
+            <div>Watching Youtobe</div>
+        </div>
+
+    );
+}
+export default TodoData;
