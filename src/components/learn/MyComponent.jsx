53d91f930c91d461a2cebf5864cc5523e303ad3f
phamvietquaan <quanpham4908@gmail.com>
2025-06-02 10:40:46 +0700

    video31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,20 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const hoidantic = "eric 1"; // string
+    // const hoidanit = 25; // number
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "quan",
+    //     age: "12"
+    // }
     return (
         <>
-            <div> eric & hoidanit</div>
-            <div className="child">child</div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit</div>
+            <div>{console.log("toi xin chao")}</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
         </>
     );
 }
